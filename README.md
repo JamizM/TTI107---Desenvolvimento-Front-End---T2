f27208cf46891cbaf0156a4cb818e76c26cf1be4
João Antônnio Martinez <133376282+JamizM@users.noreply.github.com>
2023-11-10 09:48:56 -0300

    Adição dos arquivos base para começo do desenvolvimento do trabalho TTI107 T2 (#1)

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# TTI107---Desenvolvimento-Front-End---T2
-Trabalho do T2
+<h1>WeatherApp ☀️</h1>
+
+![GitHub repo size](https://img.shields.io/github/repo-size/alexZ7000/WeatherApp?style=for-the-badge)
+![GitHub language count](https://img.shields.io/github/languages/count/alexZ7000/WeatherApp?style=for-the-badge)
+
+<img src="https://github.com/alexZ7000/WeatherApp/assets/78627928/2256edf7-a3f7-4970-8553-4820f63cd6a8" alt="WeatherApp">
+<br>
+
+> <b>WeatherApp</b> é um site que vai mostrar a temperatura de cidades/paises e suas 3 respectivas playlists mais escutadas no Spotify<br>
+>  
+<br>
+
+<h1> Avisos Importantes ⚠️</h1>
+<ul>
+    <li>Este projeto não é de minha autoria 🚫, ele foi feito pelo canal: <a href="https://www.youtube.com/watch?v=BsxGMdwFSPU"><i>ivojr</i></a></li>
+    <li>Este projeto foi feito apenas para o meu próprio aprendizado ✍🏼</li>
+</ul><br><br>
+
+
+<h1>Usando WeatherApp🤳🏼</h1>
+
+![image](https://github.com/alexZ7000/WeatherApp/assets/78627928/c357880b-cd6c-49a6-affa-410932ea354b)
+O aplicativo trabalha com a API do openweather e com a API do spotify
+
+![image](https://github.com/alexZ7000/WeatherApp/assets/78627928/8de29806-86ad-4b06-aa90-1cce484d0bce)
+Quando você digita o nome de uma cidade, a API busca a playlist mais tocada no país da respectiva cidade
+![image](https://github.com/alexZ7000/WeatherApp/assets/78627928/da74cfd1-9cf2-4909-a0e1-a53f76383a4a)
+
+
+## 🚀 Acessando WeatherApp
+
+<h3>Para acessar o <b>WeatherApp</b>, siga estas etapas:<h3><br>
+
+
+## 🤝 Colaborador
+
+
+<table>
+  <tr>
+    <td align="center">
+      <a href="#">
+        <img align="center" src="https://avatars.githubusercontent.com/u/78627928?v=4" width="100px;" alt="era uma foto daora"/><br>
+        <sub>
+          <p><b><i>Alessandro Lima:</i></b> <a href="https://github.com/alexZ7000"><i>alexZ7000</i></a>🏆</p>
+        </sub>
+      </a>
+    </td>
+</table>
---
+++ scripts.js
@@ -0,0 +1,141 @@
+let input;
+const apiKey = ``
+const clientID = ``
+const clientSecret = ``
+
+const waitDomLoad = () => {
+   input = document.getElementById("input-busca")
+   input.addEventListener('keyup', function (event){
+      if (event.keyCode === 13){
+         const valorInput = input.value;
+         movimentoInput(valorInput)
+      }
+   })
+
+   document.addEventListener("DOMContentLoaded", () => {/*Arrow function, pesquisar sobre isso*/
+      fecharInput()
+   })
+}
+
+function botaoDeBusca(){
+   const inputValue = input.value;
+   movimentoInput(inputValue)
+}
+
+
+function movimentoInput(inputValue ){
+   const visibility = document.getElementById('input-busca').style.visibility
+   inputValue && procurarCidade(inputValue);
+   visibility === 'hidden' ? abrirInput() : fecharInput(); /*if/else de uma linha só*/
+}
+
+
+
+function fecharInput(){
+   input.style.width = '40px';
+   input.style.visibility = 'hidden';
+   input.style.padding = '0.5rem 0.5rem 0.5rem 2.6rem';
+   input.style.transition = 'all 0.5s ease-in-out 0s';
+   input.value = ""; /*limpar o texto escrito pelo usuario no Entry*/
+}
+
+function abrirInput(){
+   input.style.visibility = 'visible';
+   input.style.width = '300px';
+   input.style.padding = '0.5rem 0.5rem 0.5rem 3.1rem';
+   input.style.transition = 'all 0.5s ease-in-out 0s';
+   input.value = ""; /*limpar o texto escrito pelo usuario no Entry*/
+}
+
+
+async function procurarCidade(inputValue){
+   try{
+      const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${apiKey}&units=metric&lang=pt_br`)
+      if (dados.status === 200){
+         const resultado = await dados.json();
+         await obterTopAlbunsPorPais(resultado.sys.country)
+         mostrarClimaNaTela(resultado);
+      }
+      else{
+         throw new Error
+      }
+   }
+   catch{
+      alert("A pesquisa deu errado")
+   }
+}
+
+function mostrarClimaNaTela(resultado){
+   document.querySelector('.nome-cidade').innerHTML = `${resultado.name}`;
+   document.querySelector('.temperatura').innerHTML = `${resultado.main.temp.toFixed(0)}°C`; /*a função tofixed é para determinar quantos numeros apos a virgulaqueremos, como a gnt nao quer nenhum numero apos a virgula a gente deixa o tofixed como 0*/
+   document.querySelector('.maxTemperatura').innerHTML = `Máx: ${resultado.main.temp_max.toFixed(0)}°C`;
+   document.querySelector('.minTemperatura').innerHTML = `Min: ${resultado.main.temp_min.toFixed(0)}°C`;
+   document.querySelector('.icone-tempo').src = `./images/${resultado.weather[0].icon}.png`
+}
+
+async function obterAcessoToken(){
+   const credentials = `${clientID}:${clientSecret}`;
+   const encodedCredentials = btoa(credentials);
+
+   const response = await fetch('https://accounts.spotify.com/api/token',{
+      method: "POST",
+      headers: {
+         'Authorization': `Basic ${encodedCredentials}`,
+         'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: 'grant_type=client_credentials'
+   }) //o fetch pode demorar um pouco por isso ele tem que aguardar o fetch ser executado
+   const data = await response.json()
+   return data.access_token;
+}
+
+async function obterTopAlbunsPorPais(country){
+   try {
+      const accessToken = await obterAcessoToken();
+      const dataAtual = obterDataAtual();
+      const url = `https://api.spotify.com/v1/browse/featured-playlists?country=${country}&timestamp=${dataAtual}T09%3A00%3A00&limit=3`
+      const resultado = await fetch(`${url}`, {
+         headers: {
+            'Authorization': `Bearer ${accessToken}`
+      }
+   })
+   if( resultado.status === 200){
+      const data = await resultado.json()
+      const result = data.playlists.items.map(item => ({
+               name: item.name,
+               image: item.images[0].url
+            }
+         )
+      )// o método map percorre por todos os meus objetos
+      console.log(result)
+      mostrarMusicaNaTela(result);
+   }
+   else{
+      throw new Error
+   }
+   }
+   catch {
+      alert("A pesquisa por MÚSICA deu errado!")
+   }
+}
+
+function obterDataAtual(){
+   const currentDate = new Date();
+   const ano = currentDate.getFullYear();
+   const mes = (currentDate.getMonth() + 1).toString().padStart(2, '0'); //vc soma 1 pq o método getMonth ele sempre pega o mes -1
+   // o método padStart obriga a minha pesquisa a ter 2 números, mesmo que o meu mes seja o mes 1, ele irá colocar o 01
+   const dia = currentDate.getDate().toString().padStart(2, '0')
+   return `${ano}-${mes}-${dia}`
+}
+
+const ulElement = document.querySelector('.playlist-caixa')
+const liElement = ulElement.querySelectorAll('li')//pega todos os elementos dentro da tag, e colocamos ela sem nenhum ponto por conta de ser uma tag e não uma classe
+
+function mostrarMusicaNaTela(dados){
+   liElement.forEach((liElement, index) =>{
+      const imgElement = liElement.querySelector('img');
+      const pElement = liElement.querySelector('p');
+      imgElement.src = dados[index].image;
+      pElement.textContent = dados[index].name;
+   })
+}
