2e93ec114cc817d5515276e72565de574af7bf41
alexZ7000 <78627928+alexZ7000@users.noreply.github.com>
2023-11-11 19:34:58 -0300

    Pesquisa por cidades funcionando

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-const express = require('express');
-const axios = require('axios');
-const dotenv = require('dotenv');
+require('dotenv').config();
+const express = require("express");
 const app = express();
+const https = require("https");
 
-app.use(express.json());
+const openWeatherApiKey = process.env.OPENWEATHER_API_KEY;
+const city = "Paris";
+const openWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${openWeatherApiKey}&units=metric&lang=pt_br`;
 
 app.get('/', (req, res) => {
+  https.get(openWeatherUrl, (response) => {
+    console.log(response.statusCode);
+    response.on("data", (data) => {
+      const weatherData = JSON.parse(data);
+      const temperature = weatherData.main.temp;
+      console.log(temperature);
+    });
+  });
   res.send('You are connected with the server!');
 });
 
