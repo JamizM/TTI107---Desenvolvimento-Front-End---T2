532f10232e330dbd9a98928e6bd568d8379a8d29
alexZ7000 <78627928+alexZ7000@users.noreply.github.com>
2023-11-17 14:37:47 -0300

    trabalho finalizado!

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,7 +1,7 @@
 let counter = 2;
 setInterval(() => {
     counter === 12 ? counter = 1 : counter++;
-    document.querySelector('.video').src = `../assets/videos/video${counter}.mp4`
+    document.querySelector('.video').src = `../assets/videos/video${counter}.mp4`;
 }, 12000);
 
 const getWeather = async (inputValue) => {
@@ -13,18 +13,18 @@ const getWeather = async (inputValue) => {
 
 const input = document.getElementById("input-busca");
 input.addEventListener('keyup', function (event){
-    if (event.keyCode === 13){
+    if (event.keyCode === 13) {
         const inputValue = input.value;
         isNaN(inputValue) ? getWeather(inputValue) : alert("Insira um valor válido na caixa de pesquisa");
     }
-    document.addEventListener("DOMContentLoaded", () => {/*Arrow function, pesquisar sobre isso*/
+    document.addEventListener("DOMContentLoaded", () => {
         closeInput();
-    })
+    });
 });
 
 const showWeatherOnScreen = (response) => {
     document.querySelector('.nome-cidade').innerHTML = `${response["name"]}`;
-    document.querySelector('.temperatura').innerHTML = `${response["temperature"].toFixed(0)}°C`; /*a função tofixed é para determinar quantos numeros apos a virgulaqueremos, como a gnt nao quer nenhum numero apos a virgula a gente deixa o tofixed como 0*/
+    document.querySelector('.temperatura').innerHTML = `${response["temperature"].toFixed(0)}°C`; /*a função tofixed é para determinar quantos numeros apos a virgula queremos, como a gnt nao quer nenhum numero apos a virgula a gente deixa o tofixed como 0*/
     document.querySelector('.maxTemperatura').innerHTML = `Máx: ${response["max"].toFixed(0)}°C`;
     document.querySelector('.minTemperatura').innerHTML = `Min: ${response["min"].toFixed(0)}°C`;
     document.querySelector('.latTemperatura').innerHTML = `Lat: ${response["lat"].toFixed(0)}°`;
@@ -35,20 +35,20 @@ const showWeatherOnScreen = (response) => {
 const searchButton = () => {
     const visibility = document.getElementById('input-busca').style.visibility;
     visibility === 'hidden' ? openInput() : closeInput();
-}
+};
 
-function closeInput(){
+const closeInput = () => {
     input.style.width = '40px';
     input.style.visibility = 'hidden';
     input.style.padding = '0.5rem 0.5rem 0.5rem 2.6rem';
     input.style.transition = 'all 0.5s ease-in-out 0s';
     input.value = "";
-}
+};
 
-function openInput(){
+const openInput = () => {
     input.style.visibility = 'visible';
     input.style.width = '300px';
     input.style.padding = '0.5rem 0.5rem 0.5rem 3.1rem';
     input.style.transition = 'all 0.5s ease-in-out 0s';
     input.value = "";
-}
+};
