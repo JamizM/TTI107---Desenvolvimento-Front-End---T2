0fa16a8b737ea0d29a2064a15174085fb265f819
alexZ7000 <78627928+alexZ7000@users.noreply.github.com>
2023-11-16 22:36:39 -0300

    trabalho finalizado!

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,40 @@
 require('dotenv').config();
 const express = require("express");
+const axios = require('axios');
+const cors = require('cors');
 const app = express();
 const https = require("https");
-const {response, json} = require("express");
 
 const openWeatherApiKey = process.env.OPENWEATHER_API_KEY;
+app.use(express.json());
+app.use(cors());
 
-
-
-app.get('/', (req, res) => {
-  const city = 'Brasil';//req.body
+app.post('/', (req, res) => {
+  const { city } = req.body;
   const openWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${openWeatherApiKey}&units=metric&lang=pt_br`;
   https.get(openWeatherUrl, (response) => {
     console.log(response.statusCode);
     response.on("data", async (data) => {
       try {
-        let { max_temp, min_temp, lat, lon, feels_like, description } = await getWeatherData(data);
+        const { city_name, temperature, max_temp, min_temp, lat, lon, feels_like, description, weather_image } = await getWeatherData(data);
         console.log();
-        res.send(`Temperature: { 
-          max: ${max_temp}, 
-          min: ${min_temp}, 
-          feels_like: ${feels_like}, 
-          lat: ${lat}, 
-          lon: ${lon}, 
-          description: ${description} 
-          }`).end();
+        res.send(
+            [
+                {
+                  name: city_name,
+                  temperature: temperature,
+                  max: max_temp,
+                  min: min_temp,
+                  feels_like: feels_like,
+                  lat: lat,
+                  lon: lon,
+                  description: description,
+                  weather_image: weather_image
+                }
+                ]
+        ).end();
       } catch (e) {
-        res.send(`Error while getting OpenWeather data\n error code: ${e}\n Status code: ${response.statusCode}`).end();
+        res.send(`Error while getting OpenWeather data error code: ${e} Status code: ${response.statusCode}`).end();
       }
     });
   });
@@ -34,19 +42,25 @@ app.get('/', (req, res) => {
 
 const getWeatherData = async (data) => {
   const weatherData = JSON.parse(data);
+  const city_name = weatherData.name;
+  const temperature = weatherData.main.temp;
   const max_temp = weatherData.main.temp_max;
   const min_temp = weatherData.main.temp_min;
   const lat = weatherData.coord.lat;
   const lon = weatherData.coord.lon;
   const feels_like = weatherData.main.feels_like;
-  let description = weatherData.weather[0].description;
+  const description = weatherData.weather[0].description;
+  const weather_image = weatherData.weather[0].icon;
   return {
+    city_name,
+    temperature,
     max_temp,
     min_temp,
     lat,
     lon,
     feels_like,
-    description
+    description,
+    weather_image
   };
 };
 
--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,141 +1,54 @@
-let input;
-const apiKey = ``
-const clientID = ``
-const clientSecret = ``
+let counter = 2;
+setInterval(() => {
+    counter === 12 ? counter = 1 : counter++;
+    document.querySelector('.video').src = `../assets/videos/video${counter}.mp4`
+}, 12000);
 
-const waitDomLoad = () => {
-    input = document.getElementById("input-busca")
-    input.addEventListener('keyup', function (event){
-        if (event.keyCode === 13){
-            const valorInput = input.value;
-            movimentoInput(valorInput)
-        }
-    })
+const getWeather = async (inputValue) => {
+    const serverUrl = "http://localhost:3000";
+    console.log(document.getElementById("input-busca").value);
+    const response = await axios.post(serverUrl, { city: inputValue });
+    showWeatherOnScreen(response.data[0]);
+};
 
+const input = document.getElementById("input-busca");
+input.addEventListener('keyup', function (event){
+    if (event.keyCode === 13){
+        const inputValue = input.value;
+        isNaN(inputValue) ? getWeather(inputValue) : alert("Insira um valor válido na caixa de pesquisa");
+    }
     document.addEventListener("DOMContentLoaded", () => {/*Arrow function, pesquisar sobre isso*/
-        fecharInput()
+        closeInput();
     })
+});
+
+const showWeatherOnScreen = (response) => {
+    document.querySelector('.nome-cidade').innerHTML = `${response["name"]}`;
+    document.querySelector('.temperatura').innerHTML = `${response["temperature"].toFixed(0)}°C`; /*a função tofixed é para determinar quantos numeros apos a virgulaqueremos, como a gnt nao quer nenhum numero apos a virgula a gente deixa o tofixed como 0*/
+    document.querySelector('.maxTemperatura').innerHTML = `Máx: ${response["max"].toFixed(0)}°C`;
+    document.querySelector('.minTemperatura').innerHTML = `Min: ${response["min"].toFixed(0)}°C`;
+    document.querySelector('.latTemperatura').innerHTML = `Lat: ${response["lat"].toFixed(0)}°`;
+    document.querySelector('.lonTemperatura').innerHTML = `Lon: ${response["lon"].toFixed(0)}°`;
+    document.querySelector('.icone-tempo').src = `../assets/images/${response["weather_image"]}.png`
+};
+
+const searchButton = () => {
+    const visibility = document.getElementById('input-busca').style.visibility;
+    visibility === 'hidden' ? openInput() : closeInput();
 }
 
-function botaoDeBusca(){
-    const inputValue = input.value;
-    movimentoInput(inputValue)
-}
-
-
-function movimentoInput(inputValue ){
-    const visibility = document.getElementById('input-busca').style.visibility
-    inputValue && procurarCidade(inputValue);
-    visibility === 'hidden' ? abrirInput() : fecharInput(); /*if/else de uma linha só*/
-}
-
-
-
-function fecharInput(){
+function closeInput(){
     input.style.width = '40px';
     input.style.visibility = 'hidden';
     input.style.padding = '0.5rem 0.5rem 0.5rem 2.6rem';
     input.style.transition = 'all 0.5s ease-in-out 0s';
-    input.value = ""; /*limpar o texto escrito pelo usuario no Entry*/
+    input.value = "";
 }
 
-function abrirInput(){
+function openInput(){
     input.style.visibility = 'visible';
     input.style.width = '300px';
     input.style.padding = '0.5rem 0.5rem 0.5rem 3.1rem';
     input.style.transition = 'all 0.5s ease-in-out 0s';
-    input.value = ""; /*limpar o texto escrito pelo usuario no Entry*/
-}
-
-
-async function procurarCidade(inputValue){
-    try{
-        const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${apiKey}&units=metric&lang=pt_br`)
-        if (dados.status === 200){
-            const resultado = await dados.json();
-            await obterTopAlbunsPorPais(resultado.sys.country)
-            mostrarClimaNaTela(resultado);
-        }
-        else{
-            throw new Error
-        }
-    }
-    catch{
-        alert("A pesquisa deu errado")
-    }
-}
-
-function mostrarClimaNaTela(resultado){
-    document.querySelector('.nome-cidade').innerHTML = `${resultado.name}`;
-    document.querySelector('.temperatura').innerHTML = `${resultado.main.temp.toFixed(0)}°C`; /*a função tofixed é para determinar quantos numeros apos a virgulaqueremos, como a gnt nao quer nenhum numero apos a virgula a gente deixa o tofixed como 0*/
-    document.querySelector('.maxTemperatura').innerHTML = `Máx: ${resultado.main.temp_max.toFixed(0)}°C`;
-    document.querySelector('.minTemperatura').innerHTML = `Min: ${resultado.main.temp_min.toFixed(0)}°C`;
-    document.querySelector('.icone-tempo').src = `./images/${resultado.weather[0].icon}.png`
-}
-
-async function obterAcessoToken(){
-    const credentials = `${clientID}:${clientSecret}`;
-    const encodedCredentials = btoa(credentials);
-
-    const response = await fetch('https://accounts.spotify.com/api/token',{
-        method: "POST",
-        headers: {
-            'Authorization': `Basic ${encodedCredentials}`,
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: 'grant_type=client_credentials'
-    }) //o fetch pode demorar um pouco por isso ele tem que aguardar o fetch ser executado
-    const data = await response.json()
-    return data.access_token;
-}
-
-async function obterTopAlbunsPorPais(country){
-    try {
-        const accessToken = await obterAcessoToken();
-        const dataAtual = obterDataAtual();
-        const url = `https://api.spotify.com/v1/browse/featured-playlists?country=${country}&timestamp=${dataAtual}T09%3A00%3A00&limit=3`
-        const resultado = await fetch(`${url}`, {
-            headers: {
-                'Authorization': `Bearer ${accessToken}`
-            }
-        })
-        if( resultado.status === 200){
-            const data = await resultado.json()
-            const result = data.playlists.items.map(item => ({
-                        name: item.name,
-                        image: item.images[0].url
-                    }
-                )
-            )// o método map percorre por todos os meus objetos
-            console.log(result)
-            mostrarMusicaNaTela(result);
-        }
-        else{
-            throw new Error
-        }
-    }
-    catch {
-        alert("A pesquisa por MÚSICA deu errado!")
-    }
-}
-
-function obterDataAtual(){
-    const currentDate = new Date();
-    const ano = currentDate.getFullYear();
-    const mes = (currentDate.getMonth() + 1).toString().padStart(2, '0'); //vc soma 1 pq o método getMonth ele sempre pega o mes -1
-    // o método padStart obriga a minha pesquisa a ter 2 números, mesmo que o meu mes seja o mes 1, ele irá colocar o 01
-    const dia = currentDate.getDate().toString().padStart(2, '0')
-    return `${ano}-${mes}-${dia}`
-}
-
-const ulElement = document.querySelector('.playlist-caixa')
-const liElement = ulElement.querySelectorAll('li')//pega todos os elementos dentro da tag, e colocamos ela sem nenhum ponto por conta de ser uma tag e não uma classe
-
-function mostrarMusicaNaTela(dados){
-    liElement.forEach((liElement, index) =>{
-        const imgElement = liElement.querySelector('img');
-        const pElement = liElement.querySelector('p');
-        imgElement.src = dados[index].image;
-        pElement.textContent = dados[index].name;
-    })
+    input.value = "";
 }
